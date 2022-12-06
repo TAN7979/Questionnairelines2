5cb1d0bb900087e7af70eff0a541ad9e6dbf6de9
Alexandr Timokhin <timohinan79@yandex.ru>
2022-12-06 16:24:41 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    boolean subscription;
+    public FromDate birthday;
+}
